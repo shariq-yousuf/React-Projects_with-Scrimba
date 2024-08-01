d4a7e6e0b13433508997f76e56248f7ec248b7bd
shariq-yousuf <shariqyousuf01@gmail.com>
2024-08-01 22:28:41 +0500

    chore: add navbar

--- Projects/React-info-site/src/components/Navbar.jsx
+++ Projects/React-info-site/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import reactLogo from "../assets/reactjs-icon.svg";
+
 export default function Navbar() {
   return (
-    <>
-      <h1>Navbar goes here</h1>
-    </>
+    <nav className="navbar">
+      <img src={reactLogo} alt="react-logo" className="react-logo" />
+      <h3 className="logo-text">ReactFacts</h3>
+      <h4 className="project-title">React Course - Project 1</h4>
+    </nav>
   );
 }
--- Projects/React-info-site/src/index.jsx
+++ Projects/React-info-site/src/index.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
 // import App from './App.jsx'
 // import './index.css'
 
@@ -14,4 +15,4 @@ ReactDOM.createRoot(document.querySelector("#root")).render(
   <>
     <App />
   </>
-);
+)
