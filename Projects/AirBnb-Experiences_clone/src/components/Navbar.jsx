9d7d05ca19929d39aeb9421d3fe06dc01d87d0fe
shariq-yousuf <shariqyousuf01@gmail.com>
2024-08-04 11:29:04 +0500

    chore: move code from scrimba to local and github

---
+++ Projects/AirBnb-Experiences_clone/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/airbnb-logo.png"
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={logo} alt="airbnb-logo" className="nav--logo" />
+    </nav>
+  )
+}
