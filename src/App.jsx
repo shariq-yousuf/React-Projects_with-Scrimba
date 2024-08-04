afd90c4da18214e0946936bac163045b55caeb4a
shariq-yousuf <shariqyousuf01@gmail.com>
2024-08-04 13:15:10 +0500

    chore: add card component and render it with props now

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header"
-import Main  from "./components/Main"
+import Main from "./components/Main"
 
 export default function App() {
   return (
---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import "./styles/Card.css"
+
+export default function Card(props) {
+  const { img, title, description, liveLink, github } = props
+
+  return (
+    <div className="project-container">
+      <img className="project-img" src={img} alt={`${title}-img`} />
+      <h3 className="project-title">{title}</h3>
+      <p className="project-description">{description}</p>
+      <div className="project-icons">
+        <a href={github} target="_blank">
+          <i className="fa-brands fa-github"></i>
+        </a>
+        <a href={liveLink}>
+          <i className="fa-regular fa-eye"></i>
+        </a>
+      </div>
+    </div>
+  )
+}
