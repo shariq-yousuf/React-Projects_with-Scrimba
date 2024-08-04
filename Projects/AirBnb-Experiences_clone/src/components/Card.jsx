f965407144e87259aaf3f5be9c61029c4f43c49b
shariq-yousuf <shariqyousuf01@gmail.com>
2024-08-04 11:54:34 +0500

    chore: add card first instance with hard coding

--- Projects/AirBnb-Experiences_clone/src/components/Card.jsx
+++ Projects/AirBnb-Experiences_clone/src/components/Card.jsx
@@ -23,8 +23,20 @@ Notes:
 export default function Card() {
   return (
     <div className="card">
-      <img src={katie} />
-      <img src={star} />
+      <div className="img-container">
+        <img src={katie} className="card-img" />
+        <span className="flag-tag">SOLD OUT</span>
+      </div>
+      <div className="reviews">
+        <img src={star} className="star-img" />
+        <span>
+          5.0 <span className="review-count">(6) USA</span>
+        </span>
+      </div>
+      <p>Life lessons with Katie Zaferes</p>
+      <div>
+        <span className="price">From $136</span> / person
+      </div>
     </div>
   )
 }
