f9f8da906b376d4f4958625e7be65afb9618c210
shariq-yousuf <shariqyousuf01@gmail.com>
2024-08-07 22:42:52 +0500

    chore: change props to spreading object

--- Projects/AirBnb-Experiences_clone/src/App.jsx
+++ Projects/AirBnb-Experiences_clone/src/App.jsx
@@ -6,18 +6,7 @@ import data from "./data/data"
 
 export default function App() {
   const cards = data.map((card) => {
-    return (
-      <Card
-        key={card.id}
-        img={card.coverImg}
-        rating={card.stats.rating}
-        reviewCount={card.stats.reviewCount}
-        location={card.location}
-        title={card.title}
-        price={card.price}
-        openSpots={card.openSpots}
-      />
-    )
+    return <Card key={card.id} {...card} />
   })
 
   return (
--- Projects/AirBnb-Experiences_clone/src/components/Card.jsx
+++ Projects/AirBnb-Experiences_clone/src/components/Card.jsx
@@ -1,29 +1,31 @@
 export default function Card(props) {
+  const { coverImg, stats, location, title, price, openSpots } = props
+
   let badgeText
-  if (props.openSpots === 0) {
+  if (openSpots === 0) {
     badgeText = "SOLD OUT"
-  } else if (props.location === "Online") {
+  } else if (location === "Online") {
     badgeText = "ONLINE"
   }
 
   return (
     <div className="card">
       <div className="img-container">
-        <img src={`/public/assets/${props.img}`} className="card-img" />
+        <img src={`/public/assets/${coverImg}`} className="card-img" />
         {badgeText && <span className="badge">{badgeText}</span>}
       </div>
       <div className="reviews">
         <img src={"/public/assets/star.png"} alt="star" className="star-img" />
         <span>
-          {props.rating}{" "}
+          {stats.rating}{" "}
           <span className="review-count">
-            ({props.reviewCount}) • {props.location}
+            ({stats.reviewCount}) • {location}
           </span>
         </span>
       </div>
-      <p>{props.title}</p>
+      <p>{title}</p>
       <div className="price-div">
-        <span className="price">From ${props.price}</span> / person
+        <span className="price">From ${price}</span> / person
       </div>
     </div>
   )
