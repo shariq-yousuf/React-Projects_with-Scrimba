301b4686da1f203b1e9559bfb3a65fd197cc8015
shariq-yousuf <shariqyousuf01@gmail.com>
2024-08-09 16:06:32 +0500

    chore: add header section

--- Projects/my-travel-journal/src/App.jsx
+++ Projects/my-travel-journal/src/App.jsx
@@ -1,7 +1,8 @@
 import "./styles/App.css"
+import Header from "./components/Header"
 
 export default function App() {
   return (
-    <h1>test</h1>
+    <Header />
   )
 }
---
+++ Projects/my-travel-journal/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import "../styles/Header.css"
+
+export default function Header() {
+  return (
+    <header>
+      <img className="globe-icon" src="/globe.svg" alt="globe icon" />
+      <h2 className="header-title">my travel journal.</h2>
+    </header>
+  )
+}
