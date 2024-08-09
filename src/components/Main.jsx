777e853f02eb229873fb3945c5e0115ea03f6b5e
shariq-yousuf <shariqyousuf01@gmail.com>
2024-08-09 21:17:44 +0500

    add my-travel-journal app to homepage, and modify card styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,13 +3,9 @@ import Card from "./Card"
 import data from "../data/data.json"
 
 export default function Main() {
-  const cards = data.map((card) => {
+  const cards = data.reverse().map((card) => {
     return <Card key={card.id} {...card} />
   })
 
-  return (
-    <main>
-      {cards}
-    </main>
-  )
+  return <main>{cards}</main>
 }
